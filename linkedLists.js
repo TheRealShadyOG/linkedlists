184c8c0d98415d199c1727a8603bdd0269366a1c
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-30 08:12:24 -0500

    Add append function to linkedList factory

--- linkedLists.js
+++ linkedLists.js
@@ -1,10 +1,35 @@
 // Factory for link lists
-const linkedList = () => {};
+const linkedList = () => {
+  let head = null;
+  // Function to add new node containing value to end of list
+  const append = (value) => {
+    // Create new node
+    const newNode = node(value);
+    // If list doesnt have head, make node head
+    if (head === null) {
+      head = newNode;
+    } else {
+      // Store head
+      let current = head;
+      // Loop until current last node
+      while (current.next) {
+        current = current.next;
+      }
+      // Append node to last place
+      current.next = newNode;
+    }
+  };
+
+  return {
+    append,
+    show,
+  };
+};
 
 // Factory for node
-const node = (value = null, nextNode = null) => {
+const node = (value = null, next = null) => {
   return {
     value,
-    nextNode,
+    next,
   };
 };
